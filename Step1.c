1a5338e34878b5cbd8461c3c8c1514d70c67f8e7
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-11-20 18:40:26 +0100

    c program

---
+++ Step1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int var1 = 7;
+    int var2 = 1;
+
+    if (var1 + var2 == 8) {
+        puts("Normal.");
+    } else {
+        puts("Hmm wait what ?!");
+    }
+    return var2;
+}
