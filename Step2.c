7d1aa2fad7c206f94cfc9fc97240bd723a24780b
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-11-20 20:11:47 +0100

    key 1 found

--- Step2.c
+++ Step2.c
@@ -1,9 +1,19 @@
 
-int step1(char *value) {
+
+void step2(){
+
+}
+
+void step1(char *value) {
     //puts("Value can't be converted to integer.");
     if (strlen(value) != 5) {
         return;
     }
+    if (value[0] != 'P') return;
+    if (value[1] != 'o') return;
+    if (value[2] != 'C') return;
+    if (value[3] != '<') return;
+    if (value[4] != '3') return;
     printf("Format string + %s", value);
     step2();
 }
