9835e2a1874e0debba5085b90fb1be7caf1f4964
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-11-20 19:54:25 +0100

    Not sure about the messages being displayed

--- Step2.c
+++ Step2.c
@@ -1,12 +1,21 @@
 
+int step1(char *value) {
+    //puts("Value can't be converted to integer.");
+    if (strlen(value) != 5) {
+        return;
+    }
+    printf("Format string + %s", value);
+    step2();
+}
+
 
 int main(int argc, char**argv) {
 
-    puts("Value can't be converted to integer.");
+    //puts("Value can't be converted to integer.");
     if (argc != 2) {
         return 1;
     }
     step1(argv[1]);
-    puts("Value can't be converted to integer.");
+    //puts("Value can't be converted to integer.");
     return 0;
 }
