2447653bbeb04f22e352bf846a3a5c7f0dbeb1f3
MsteR7 <ester.ngindu-mubiala@epitech.eu>
2025-11-20 19:43:12 +0100

    Getting the main of step2

--- Step2.c
+++ Step2.c
@@ -1,7 +1,12 @@
 
 
-int main() {
+int main(int argc, char**argv) {
+
     puts("Value can't be converted to integer.");
+    if (argc != 2) {
+        return 1;
+    }
+    step1(argv[1]);
     puts("Value can't be converted to integer.");
-    return;
+    return 0;
 }
